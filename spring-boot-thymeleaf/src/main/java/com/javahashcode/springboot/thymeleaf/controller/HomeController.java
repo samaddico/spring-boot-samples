f063687917e74a4bea925883f0b77defea4a752f
samuel.addico <samuel.addico@dreamoval.com>
2018-05-05 11:54:44 +0000

    Added source for thymleaf tutorial

---
+++ spring-boot-thymeleaf/src/main/java/com/javahashcode/springboot/thymeleaf/controller/HomeController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.javahashcode.springboot.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author addico
+ */
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+    
+    @RequestMapping("/about")
+    public String about(){
+        return "about";
+    }
+    
+    @RequestMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+}

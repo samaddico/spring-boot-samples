ca18467600af26ab8b6075615604266e2bcaa975
samuel.addico <samuel.addico@dreamoval.com>
2018-06-18 04:38:36 +0000

    Added Spring Boot Tomcat SSL support

---
+++ spring-boot-tomcat-ssl-example/src/test/java/com/samaddico/springboottomcatsslexample/SpringBootTomcatSslExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.samaddico.springboottomcatsslexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootTomcatSslExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

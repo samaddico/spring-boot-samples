fa29d426da0e28befb706d0896dd6cd1aec428da
Samuel Anertey Addico <samaddico@gmail.com>
2018-09-19 14:39:11 +0000

    Added tutorial source code for spring log to cloudwatch

---
+++ spring-boot-logging-cloudwatch/src/main/java/com/samaddico/springboot/cloudwatchlog/SpringBootCloudwatch.java
@@ -0,0 +1,13 @@
+package com.samaddico.springboot.cloudwatchlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudwatch {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCloudwatch.class, args);
+    }
+
+}

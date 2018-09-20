92182db00a70ffdf1a62a79c87ee95f263b90a48
Samuel Anertey Addico <samaddico@gmail.com>
2018-09-20 10:55:38 +0000

    Added source for example log to logentries

---
+++ spring-boot-logging-logenteries/src/main/java/com/samaddico/springboot/logging/logenteries/SpringBootLogentriesMain.java
@@ -0,0 +1,12 @@
+package com.samaddico.springboot.logging.logenteries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogentriesMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLogentriesMain.class, args);
+    }
+}

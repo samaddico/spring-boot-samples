b640c7f7eb71c2235875e874dd4e1934d560ec34
Samuel Anertey Addico <samaddico@gmail.com>
2018-09-21 13:56:16 +0000

    [WIP] sample project on how to push and pull objects from aws s3

---
+++ spring-boot-aws-s3/src/main/java/com/samaddico/springboot/aws/s3/SpringBootAwsS3Application.java
@@ -0,0 +1,12 @@
+package com.samaddico.springboot.aws.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAwsS3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAwsS3Application.class, args);
+    }
+}

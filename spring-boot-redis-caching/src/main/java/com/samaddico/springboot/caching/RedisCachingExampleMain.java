c2aa6ba5ec0fc20c453e223dbff2f26fd685552d
Samuel Anertey Addico <samaddico@gmail.com>
2018-10-02 16:05:27 +0000

    Removed RestTemplate Bean

--- spring-boot-redis-caching/src/main/java/com/samaddico/springboot/caching/RedisCachingExampleMain.java
+++ spring-boot-redis-caching/src/main/java/com/samaddico/springboot/caching/RedisCachingExampleMain.java
@@ -3,11 +3,6 @@ package com.samaddico.springboot.caching;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @EnableCaching()
 @SpringBootApplication
@@ -17,12 +12,4 @@ public class RedisCachingExampleMain {
         SpringApplication.run(RedisCachingExampleMain.class, args);
     }
 
-    @Bean
-    public <T> RedisTemplate<String, T> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, T> template = new RedisTemplate<String, T>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return template;
-    }
 }

554b5ee7f9df91a80574b8c4c7e356254c326b03
Samuel Anertey Addico <samaddico@gmail.com>
2018-10-10 17:22:04 +0000

    code for spring boot rabbitmq example

---
+++ springboot-rabbitmq-example/src/main/java/com/samaddico/springboot/rabbitmq/SpringBootRabbitmqExample.java
@@ -0,0 +1,12 @@
+package com.samaddico.springboot.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqExample {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqExample.class, args);
+    }
+}
